a1cf6566b47e13105829c34997ae330928e05351
Robson Vieira <robsonvieira97@gmail.com>
2023-06-04 22:40:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
-const INPUT_TASK = document.getElementById("input_task");
-const LIST = document.getElementById("list");
-const DATE = document.getElementById("date");
+const inputTask = document.getElementById("input_task");
+const list = document.getElementById("list");
+const today = document.getElementById("date");
+
+const tasks = [
+  {
+    title: "teste de task",
+  },
+  {
+    title: "teste de task 2",
+  },
+];
 
 var tasks_count = 0;
 
-function add_task() {
-  let title = INPUT_TASK.querySelector("input").value;
-  
-  if(title === "") return false;
-  
-  LIST.appendChild(create_task(title));
+const updateBoard = () => {
+  let htmlList = tasks.map((e, i) => createTask(e, i));
+
+  console.log(htmlList);
+
+  list.innerText = "";
+  list.append(...htmlList);
+};
+
+function addTask() {
+  let title = inputTask.querySelector("input").value;
+
+  if (title === "") return false;
+
+  tasks.push({ title });
+
+  updateBoard(tasks);
   toggleList();
-  INPUT_TASK.querySelector("input").value = "";
+  inputTask.querySelector("input").value = "";
 }
 
-function create_task(title) {
+const createTask = (task, index) => {
   let div = document.createElement("div");
 
   tasks_count++;
 
   div.setAttribute("class", "task");
-  div.setAttribute("data-id", `task-${tasks_count}`);
+  div.setAttribute("draggable", "true");
 
   div.innerHTML = `
-    <span>${title}</span>
+    <span>${task.title}</span>
     <div class="buttons">
       <button><i class="fa-solid fa-check"></i></button>
       <button><i class="fa-solid fa-xmark"></i></button>
@@ -37,21 +57,30 @@ function create_task(title) {
   };
   buttons[1].onclick = () => {
     div.parentNode.removeChild(div);
+    tasks.splice(index, 1);
     toggleList();
   };
 
   return div;
-}
+};
 
-function toggleList() {
-  if (LIST.childElementCount <= 0) {
-    LIST.setAttribute("style", "display:none;");
+const toggleList = () => {
+  if (list.childElementCount <= 0) {
+    list.setAttribute("style", "display:none;");
   } else {
-    LIST.setAttribute("style", "");
+    list.setAttribute("style", "");
   }
-}
+};
 
-var date = new Date(Date.now());
+const moveArrayElement = (arr, from, to) => {
+  let el = arr[from];
+  arr.splice(from, 1);
+  arr.splice(to, 0, el);
+};
 
-DATE.innerText = date.toLocaleDateString();
-INPUT_TASK.querySelector("button").onclick = add_task;
+list.addEventListener("dragover", (e) => {
+ console.log(e.clientY);
+})
+
+today.innerText = new Date(Date.now()).toLocaleDateString();
+inputTask.querySelector("button").onclick = addTask;
